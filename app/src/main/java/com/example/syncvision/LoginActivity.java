2fce5f6209281e7237fe6f48bdb1e74deb87707e
ViniyXtr <lorvinlonginus@gmail.com>
2024-06-18 12:40:02 +0300

    Added Acivity lifecycle implementation

--- app/src/main/java/com/example/syncvision/LoginActivity.java
+++ app/src/main/java/com/example/syncvision/LoginActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     SwitchCompat switchMode;
     boolean nightMode;
     SharedPreferences sharedPreferences;
@@ -30,6 +33,9 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Implementation of the onCreate life cycle activity
+        Log.d(TAG, "OnCreate called");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
 
         switchMode = findViewById(R.id.switchModeLogin);
         sharedPreferences = getSharedPreferences( "MODE", Context.MODE_PRIVATE);
@@ -63,5 +69,53 @@ public class LoginActivity extends AppCompatActivity {
             }
 
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+        //The activity is about to become visible
+        //Example: Start animations, refresh UI elements
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume called");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        //The activity has become visible (it is now "resumed")
+        //Example: Resume paused operations like a video or game
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        //Another activity is taking focus (this activity is about to be "paused")
+        //Example: Pause ongoing actions (e.g, animations, video playback)
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        //The activity is no longer visible (It is now "stopped")
+        //Example: Release resources, save data to persistent storage
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        //The activity is about to be destroyed
+        //Example: Clean up resources, finalize operations
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart called");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+        //The activity is about to be restarted after it was stopped
+        //Example: Re-initialize components that were released in onStop()
     }
 }
